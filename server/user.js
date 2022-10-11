ab742e66e0d6d675dce148894493f0450596f15d
Masami Ogoshi <ogochan@wasp.co.jp>
2022-10-11 14:25:08 +0000

    URL rule

--- server/user.js
+++ server/user.js
@@ -33,7 +33,7 @@ const   getProfile = (user, profile_name) => {
             return  new Promise((done, fail) => {
                 let profile = Object({
                     name: 'default',
-                    path: makeDefaultPath(user, MY_DOMAIN);
+                    path: makeDefaultPath(user, MY_DOMAIN)
                 });
                 done(profile);
             });
